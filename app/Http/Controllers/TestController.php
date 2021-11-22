cc992d15ea90e2a9fc8bd646de061a53c6985949
Maxim Bezrukavnikov <dmntr@rambler.ru>
2021-11-22 21:57:30 +1000

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function test(Request $request)
+    {
+        return response()->json(['foo' => 'bar']);
+    }
 }
