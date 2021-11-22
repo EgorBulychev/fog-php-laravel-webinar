50eb1f87474dbca044c703654d378aad6e002270
Maxim Bezrukavnikov <dmntr@rambler.ru>
2021-11-23 00:06:46 +1000

    UsersTableSeeder fix

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,20 +1,24 @@
 <?php
 
-class UsersTableSeeder
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
 {
     public function run()
     {
+        factory(App\User::class, 20)->create();
+
         // todo разобраться с сидерами и фабриками
         // https://laravel.com/docs/5.5/seeding
-        if (config('app.env') == 'production') {
-            DB::table('users')->insert([
-                'name' => str_random(10),
-                'email' => str_random(10).'@gmail.com',
-                'password' => bcrypt('secret'),
-            ]);
-        }
-        if (config('app.env') == 'develop') {
-            factory();
-        }
+        // if (config('app.env') == 'production') {
+        //     DB::table('users')->insert([
+        //         'name' => str_random(10),
+        //         'email' => str_random(10).'@gmail.com',
+        //         'password' => bcrypt('secret'),
+        //     ]);
+        // }
+        // if (config('app.env') == 'develop') {
+        //     factory();
+        // }
     }
 }
