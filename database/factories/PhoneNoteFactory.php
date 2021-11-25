c5a5b463ffe75a3c3eb9816bd7238c29586bdec6
Maxim Bezrukavnikov <dmntr@rambler.ru>
2021-11-25 22:19:57 +1000

    Создание сидера PhoneNotesSeeder
    + создание фабрики PhoneNoteFactory

--- database/factories/PhoneNoteFactory.php
+++ database/factories/PhoneNoteFactory.php
@@ -3,7 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\PhoneNote::class, function (Faker $faker) {
+    $users = \App\User::all();
+
     return [
-        //
+        'name'    => $faker->name,
+        'number'  => $faker->phoneNumber,
+        'user_id' => rand(1, $users->count()),
     ];
 });
